8b82fda66fc285afa497e8aba5611e7fdb185525
sayanpal469 <sayanpal469@gmail.com>
2026-07-07 17:25:02 +0530

    t

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,7 @@
 export const isProduction = true
 
 const LOCAL_API_URL = 'http://localhost:5000/api'
-const PRODUCTION_API_URL = 'https://darkslategrey-rook-627450.hostingersite.com/api'
+// const PRODUCTION_API_URL = 'https://darkslategrey-rook-627450.hostingersite.com/api'
+const PRODUCTION_API_URL = 'https://skiri-portfolio.onrender.com/api'
 
 export const API_URL = isProduction ? PRODUCTION_API_URL : LOCAL_API_URL
